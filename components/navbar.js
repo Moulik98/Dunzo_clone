90ac1848837849eb5dfb111257cd588e0bb29298
Tejaswini Khengare <97454567+Tejaswini179@users.noreply.github.com>
2022-02-27 12:19:05 +0530

    completed merging

--- components/navbar.js
+++ components/navbar.js
@@ -5,7 +5,8 @@ function navbar(){
             <img class="dunzo-img" onclick="window.location.href=''" src="https://resources.dunzo.com/web-assets/prod/_next/static/images/logo-4d2d81aefcf296bc100d9edc114c2ea5.png" alt="">
         </div>
         <div class="location-div">
-            <img id="location-icon" src="https://www.pinclipart.com/picdir/big/391-3913630_new-training-location-green-location-icon-png-clipart.png" alt="">
+        <span>
+            <img id="location-icon" src="https://www.pinclipart.com/picdir/big/391-3913630_new-training-location-green-location-icon-png-clipart.png" alt=""></span>
             <p class="location-tag" onclick="window.location.href=''">Set Location</p>
         </div>
     </div> 
@@ -17,7 +18,7 @@ function navbar(){
         <p class="Hover-class">Business with Dunzo</p>
     </div>
     <div class="search-div">
-        <img id="search-icon" src="https://th.bing.com/th/id/OIP.UcEnmjkins9Evx4EGa6PIgHaHa?w=213&h=213&c=7&r=0&o=5&dpr=1.25&pid=1.7" alt="">
+        <img id="search-icon" src="https://media.istockphoto.com/vectors/icon-search-logo-design-universal-business-social-media-vector-id995577016?k=6&m=995577016&s=170667a&w=0&h=CmeJfTL2uLuleqE-gmJ7nFC1zKE3q9QakSCZXAf0Wzs=" alt="">
         <p class="Hover-class">Search</p>
     </div>
     <div class="cart-div">
@@ -25,7 +26,7 @@ function navbar(){
         <p class="Hover-class">Cart</p>
     </div>
     <div class="signin-div">
-        <button id="signIn-button">Sign in</button>
+    <a href="./signin.html"><button id="signIn-button">Sign in</button></a>
     </div>
     </div>
     
